f97e4fceadd22c9a6afedb3ddba1f53b61cf1453
Peter Krooks <pekr@Peters-MacBook-Pro.local>
2023-01-29 15:48:31 +0100

    fler bilder

--- app.py
+++ app.py
@@ -4,7 +4,9 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    images = ["/static/images/image1.jpg", "/static/images/image2.jpg", "/static/images/image3.jpg"]
+    images = ["/static/images/image1.jpg", "/static/images/image2.jpg", "/static/images/image3.jpg",
+              "/static/images/image4.jpg", "/static/images/image5.jpg", "/static/images/image6.jpg",
+              "/static/images/image7.jpg", "/static/images/image8.jpg"]
     return render_template('index.html', images=images)
 
 if __name__ == '__main__':
