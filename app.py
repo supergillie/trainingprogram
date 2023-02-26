0b14e098749025a6c4847f7d6122ce18532155c5
Peter Krooks <pekr@Peters-MacBook-Pro.local>
2023-02-26 10:19:37 +0100

    bulletlist

--- app.py
+++ app.py
@@ -4,10 +4,7 @@ app = Flask(__name__, static_url_path='/static')
 
 @app.route('/')
 def index():
-    images = ["/static/images/image1.jpg", "/static/images/image2.jpg", "/static/images/image3.jpg",
-              "/static/images/image4.jpg", "/static/images/image5.jpg", "/static/images/image6.jpg",
-              "/static/images/image7.jpg", "/static/images/image8.jpg"]
-    return render_template('index.html', images=images)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
